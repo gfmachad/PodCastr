701f47ec68c197d18fce1a839809773315a84d57
Guilherme Fritzen Machado <greer_13@hotmail.com>
2021-04-24 15:12:14 -0300

    configure connection with deployed fake api

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,7 +1,7 @@
 //será importado sem o default pq ajuda na parte do autoimport do vscode e na responsividade (mas os itens da pasta pages não podem ser exportados assim)
 import format from 'date-fns/format';
 import ptBR from 'date-fns/locale/pt-BR'; //para ver os formatos https://date-fns.org/v2.21.1/docs/format
-import Link from 'next/Link';
+
 import styles from './styles.module.scss';
 
 export function Header() {
@@ -11,11 +11,9 @@ export function Header() {
 
   return (
     <header className={styles.headerContainer}>
-      <Link href="/">
-        <a>
-          <img src="/logo.svg" alt="PodCastr" />
-        </a>
-      </Link>
+      <a href="/">
+        <img src="/logo.svg" alt="PodCastr" />
+      </a>
 
       <p>O Melhor para você ouvir</p>
 
